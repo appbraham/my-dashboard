b60c1a4e6b9a65c107c278ddefda70a1dd42d73a
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-17 10:51:04 -0500

    feat: add project structure

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users-loader',
+  standalone: true,
+  imports:[],
+  template: `<h1>Hola Mundo Users Loaders</h1>`,
+})
+
+export class UserLoaderComponent {
+
+}
